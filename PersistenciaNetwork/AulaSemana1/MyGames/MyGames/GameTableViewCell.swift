149f59182f1781188c6506850969ad7951dfdbc5
PedroBorba <pedro.lp.1990@gmail.com>
2020-05-26 01:17:10 -0300

    Ajuste do Autor do Commit Anterior

--- PersistenciaNetwork/AulaSemana1/MyGames/MyGames/GameTableViewCell.swift
+++ PersistenciaNetwork/AulaSemana1/MyGames/MyGames/GameTableViewCell.swift
@@ -15,7 +15,6 @@ class GameTableViewCell: UITableViewCell {
     @IBOutlet weak var lbTitle: UILabel!
     @IBOutlet weak var lbConsole: UILabel!
     
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
